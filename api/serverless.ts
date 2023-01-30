52ff8fe8f054f9bb00771dd6019aec385c0cf645
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-01-30 22:12:20 +0100

    [TEST]

--- api/serverless.ts
+++ api/serverless.ts
@@ -4,21 +4,11 @@ dotenv.config();
 // Require the framework
 import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 
-import { Telegraf } from "telegraf";
 
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
-const telegramWebhook = bot.createWebhook({ domain: process.env.TELEGRAM_WEBHOOK_DOMAIN! });
 // Instantiate Fastify with some config
 const app = Fastify({
   logger: false,
 });
-
-
-app.post(bot.secretPathComponent(), async (req,res) => {
-     (await telegramWebhook)(req.raw, res.raw)
-})
-
-bot.on("message", ctx => ctx.reply("Hello"));
 
 
 // Register your application as a normal plugin.
--- functions/index.ts
+++ functions/index.ts
@@ -18,7 +18,19 @@ interface CustomRouteGenericQuery {
     Querystring: IQueryString
 }
 
+
+import { Telegraf } from "telegraf";
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
+const telegramWebhook = bot.createWebhook({ domain: process.env.TELEGRAM_WEBHOOK_DOMAIN! });
+
+bot.on("message", ctx => ctx.reply("Hello"));
+
 export default async function (instance: FastifyInstance, opts: FastifyServerOptions, done:any) {
+
+    instance.post(bot.secretPathComponent(), async (req,res) => {
+            (await telegramWebhook)(req.raw, res.raw)
+    })
+    
 
     // This is parse payload as "raw". required for Stripe, but means if you need JSON you need to JSON.parse before ...
     instance.addContentTypeParser('application/json', { parseAs: 'string' }, function (req, body, done) { done(null, body); })
