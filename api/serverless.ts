80b5d6182d06ef261a390adefe991e81740bf019
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-01-30 22:47:16 +0100

    [TEST]

--- api/serverless.ts
+++ api/serverless.ts
@@ -5,12 +5,14 @@ dotenv.config();
 import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 
 
+
 // Instantiate Fastify with some config
 const app = Fastify({
   logger: false,
 });
 
 // Understand webhooks with telegram API : https://strapengine.com/telegram-bot-webhook-tutorial/
+// https://www.marclittlemore.com/serverless-telegram-chatbot-vercel/
 import { Telegraf } from "telegraf";
 export const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
 
@@ -22,9 +24,9 @@ app.register(import("../functions/index"), {
 
 
 export default async (req: FastifyRequest<any>, res: FastifyReply) => {
+    await bot.createWebhook({ domain: process.env.TELEGRAM_WEBHOOK_DOMAIN!, path:"/telegram" });
 
     await app.ready();
-    await bot.createWebhook({ domain: process.env.TELEGRAM_WEBHOOK_DOMAIN!, path:"/telegram" });
 
     app.server.emit('request', req, res);
 }
