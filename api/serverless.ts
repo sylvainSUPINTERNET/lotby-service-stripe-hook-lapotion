9ac35193cb7c8d661593e69102f7ebd3219666cc
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-02-03 23:54:02 +0100

    [d]

--- api/serverless.ts
+++ api/serverless.ts
@@ -32,8 +32,8 @@ export default async (req: FastifyRequest<any>, res: FastifyReply) => {
   console.log("exddd" , process.env.VERCEL_URL)
 
   // Since we have a free plan on vercel, we need to set the webhook dynamically ( new url for each build ) 
-  let url = `https://${process.env.VERCEL_URL}/telegram`
-  
+  // let url = `https://${process.env.VERCEL_URL}/telegram`
+    let url = `https://lotby-service-stripe-hook-lapotion.vercel.app/telegram`
   try {
     await axios.get(`https://api.telegram.org/bot${telegramBotToken}/setWebhook?url=${url}`);
   } catch ( e ) {
