832f6216cd6067c4bd67d763feb6d82761cd47fe
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-02-04 02:19:48 +0100

    [e]

--- services/botService.ts
+++ services/botService.ts
@@ -9,15 +9,9 @@ export const applyCmd = async ( message:IMessageFromTelegram, telegramToken:stri
     const bot = new Telegraf(telegramToken);
 
     if ( msg[0] === "/start" ) {
-        console.log("SHHH")
-
         if ( msg.length > 1 ) {
-            console.log("SHHH")
-
             if ( msg[1] === process.env.BOT_PASSWORD ) {
-                console.log("SHHH", message.message.chat.id)
-                bot.telegram.sendMessage(message.message.chat.id, '**TODO**  SIPEI', { parse_mode: 'MarkdownV2' });
-                // TODO save user in database with chatId ( upsert )
+                bot.telegram.sendMessage(message.message.chat.id, '__TODO__  SIPEI', { parse_mode: 'MarkdownV2' });
             }
         } 
     }
