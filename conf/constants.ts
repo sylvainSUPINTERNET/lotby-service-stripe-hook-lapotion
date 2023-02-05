25dbe180e9792e9e95b3aaf942c5c8aa882ebec2
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-02-05 19:43:20 +0100

    [UPDATE]

---
+++ conf/constants.ts
@@ -0,0 +1,2 @@
+export const DB_NAME="lotby-pusher";
+export const USERS_COLLECTION="users";
--- services/botService.ts
+++ services/botService.ts
@@ -1,17 +1,42 @@
 import axios from "axios";
 import { Telegraf } from "telegraf";
 import { IMessageFromTelegram, TELEGRAM_API } from "../functions"
-
+import { MongoClient } from "mongodb";
+import { DB_NAME, USERS_COLLECTION } from "../conf/constants";
 
 export const applyCmd = async ( message:IMessageFromTelegram, telegramToken:string  ): Promise<void> =>  {
     let msg = message.message.text.split(" ");
 
     const bot = new Telegraf(telegramToken);
 
-    if ( msg[0] === "/start" ) {
+    if ( msg[0] === "/subscribe" ) {
         if ( msg.length > 1 ) {
             if ( msg[1] === process.env.BOT_PASSWORD ) {
-                bot.telegram.sendMessage(message.message.chat.id, '__TODO__  SIPEI', { parse_mode: 'MarkdownV2' });
+                // mongodb+srv://user1234:user1234@cluster0.vhh4x.mongodb.net/?retryWrites=true&w=majority
+                try {
+                    const client = await MongoClient.connect(process.env.DB_CONN_STRING!);
+                    const userCollection = client.db(DB_NAME).collection(USERS_COLLECTION);
+                    const session = client.startSession();
+                    session.startTransaction();
+                    const query = { "chatId": message.message.chat.id }
+                    const update = {
+                        $set: {
+                            "chatId": message.message.chat.id
+                        }
+                    }
+                    const option = {
+                        upsert: true,
+                        session
+                    }
+                    await userCollection.updateOne(query, update, option);
+                    await session.endSession();
+                    client.close();
+                    bot.telegram.sendMessage(message.message.chat.id, 'Added with success !', { parse_mode: 'MarkdownV2' });
+                } catch ( e ) {
+                    console.log(e);
+                    bot.telegram.sendMessage(message.message.chat.id, `${e}`, { parse_mode: 'MarkdownV2' });
+                }
+
             }
         } 
     }
